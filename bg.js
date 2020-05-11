9559a0be3cf140e7e4b892af7e6a2a05b75942e8
Prarty <ptpratibhatiwari@gmail.com>
2020-05-12 00:09:20 +0530

    Hello world extension

---
+++ bg.js
@@ -0,0 +1,13 @@
+chrome.runtime.onInstalled.addListener(function() {
+ chrome.contextMenus.create({
+      "id": "sampleContextMenu",
+      "title": "Sample Context Menu",
+      "contexts": ["selection"]
+    });
+  });
+
+  // This will run when a bookmark is created.
+//chrome.bookmarks.onCreated.addListener(function() {
+//    // do something
+//      alert("This is my favorite website!");
+//  });
